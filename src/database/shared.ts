9999a9a6c21c8b2b8de6a71f3efcc9181b949755
Henry Schimke <henry@azule.info>
2022-02-14 17:48:26 +0000

    remove ts files, add building but untested docker

--- src/database/shared.ts
+++
@@ -1,8 +0,0 @@
-function sqlToString(sql: string, values?: Array<string | number | boolean | null>): string {
-    const value_str = values !== undefined ? values.map((val) => {
-        return `[value: ${val} type: ${typeof (val)}] `;
-    }) : '';
-    return `${sql} : ${value_str}`;
-}
-
-export {sqlToString}
--- src/done/shared-constants.ts
+++
@@ -1,3 +0,0 @@
-const ALL_PROFESSIONS: Array<CharacterProfession> = ['Jewelcrafting', 'Tailoring', 'Alchemy', 'Herbalism', 'Inscription', 'Enchanting', 'Blacksmithing', 'Mining', 'Engineering', 'Leatherworking', 'Skinning', 'Cooking'];
-
-export { ALL_PROFESSIONS };

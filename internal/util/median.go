627b6b5a291588c762e6e5e5b3b2d8f26e1eacc8
Henry Schimke <henry@azule.info>
2022-03-05 20:46:45 +0000

    median tests and fixes

--- internal/util/median.go
+++ internal/util/median.go
@@ -2,16 +2,16 @@ package util
 
 import (
 	"fmt"
-	"math"
 	"sort"
 )
 
 func Median(array []float64) (float64, error) {
-	sortedArray := sort.Float64Slice(array)
+	sortedArray := array //sort.Float64Slice(array)
+	sort.Float64s(sortedArray)
 	length := len(sortedArray)
 	var median float64
 	if length == 0 {
-		return math.NaN(), fmt.Errorf("array cannot be empty")
+		return 0, fmt.Errorf("array cannot be empty")
 	} else if length%2 == 0 {
 		middleNumbers := sortedArray[length/2-1 : length/2+1]
 		median = (middleNumbers[0] + middleNumbers[1]) / 2
@@ -23,19 +23,26 @@ func Median(array []float64) (float64, error) {
 
 func MedianFromMap(source map[float64]uint64) (float64, error) {
 	if len(source) == 0 {
-		return math.NaN(), fmt.Errorf("array cannot be empty")
+		return 0, fmt.Errorf("array cannot be empty")
 	}
 	sum := uint64(0)
 	for _, value := range source {
 		sum += value
 	}
-	useMiddle := sum%2 != 0
+	useMiddle := sum%2 == 0
 
 	var returnValue float64
 
+	//setupMap
+	keys := make([]float64, 0, len(source))
+	for key := range source {
+		keys = append(keys, key)
+	}
+	sort.Float64s(keys)
+
 	if useMiddle {
 		target1 := sum/2 - 1
-		target2 := sum/2 + 1
+		target2 := sum / 2
 
 		pickup1 := float64(0)
 		pickup2 := float64(0)
@@ -44,8 +51,10 @@ func MedianFromMap(source map[float64]uint64) (float64, error) {
 		found2 := false
 
 		runningTotal := uint64(0)
-		for key, value := range source {
+		for _, key := range keys {
+			value := source[key]
 			runningTotal += value
+
 			if runningTotal >= target1 && !found1 {
 				pickup1 = key
 				found1 = true
@@ -63,7 +72,8 @@ func MedianFromMap(source map[float64]uint64) (float64, error) {
 		target := sum / 2
 
 		runningTotal := uint64(0)
-		for key, value := range source {
+		for _, key := range keys {
+			value := source[key]
 			runningTotal += value
 			if runningTotal >= target {
 				returnValue = key
---
+++ internal/util/median_test.go
@@ -0,0 +1,217 @@
+package util
+
+import (
+	"testing"
+)
+
+func TestMedian(t *testing.T) {
+	type args struct {
+		array []float64
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    float64
+		wantErr bool
+	}{
+		{name: "odd",
+			args: args{
+				array: []float64{34, 46, 46, 23, 3, 23, 3, 3, 3356.56, 565, 3, 4, 4, 65, 7, 7, 6, 34, 5, 47, 57, 35, 6, 345, 4, 5, 7, 5, 65, 345, 345, 4, 5, 7, 456, 456, 575, 47, 57, 45, 64, 5, 436, 45, 7, 457, 4, 56, 45, 7, 45, 74, 564, 56, 45, 7, 457, 46, 453, 45, 4},
+			},
+			want:    45,
+			wantErr: false,
+		},
+		{name: "even",
+			args: args{
+				array: []float64{34, 46, 9, 46, 23, 3, 23, 3, 3, 3356.56, 565, 3, 4, 4, 65, 7, 7, 6, 34, 5, 47, 57, 35, 6, 345, 4, 5, 7, 5, 65, 345, 345, 4, 5, 7, 456, 456, 575, 47, 57, 45, 64, 5, 436, 45, 7, 457, 4, 56, 45, 7, 45, 74, 564, 56, 45, 7, 457, 46, 453, 45, 4},
+			},
+			want:    45,
+			wantErr: false,
+		},
+		{
+			name: "1",
+			args: args{
+				array: []float64{5},
+			},
+			want:    5,
+			wantErr: false,
+		},
+		{
+			name: "2",
+			args: args{
+				array: []float64{5, 5},
+			},
+			want:    5,
+			wantErr: false,
+		},
+		{
+			name: "2 - dif",
+			args: args{
+				array: []float64{5, 7},
+			},
+			want:    6,
+			wantErr: false,
+		},
+		{name: "empty",
+			args: args{
+				array: []float64{},
+			},
+			want:    0,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := Median(tt.args.array)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Median() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Median() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMedianFromMap(t *testing.T) {
+	type args struct {
+		source map[float64]uint64
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    float64
+		wantErr bool
+	}{
+		{
+			name: "empty",
+			args: args{
+				source: nil,
+			},
+			want:    0,
+			wantErr: true,
+		},
+		{
+			name: "empty",
+			args: args{
+				source: make(map[float64]uint64),
+			},
+			want:    0,
+			wantErr: true,
+		},
+		{
+			name: "one odd",
+			args: args{
+				source: map[float64]uint64{
+					5: 5,
+				},
+			},
+			want:    5,
+			wantErr: false,
+		},
+		{
+			name: "one even",
+			args: args{
+				source: map[float64]uint64{
+					5: 6,
+				},
+			},
+			want:    5,
+			wantErr: false,
+		},
+		{
+			name: "one",
+			args: args{
+				source: map[float64]uint64{
+					5: 5,
+					7: 5,
+				},
+			},
+			want:    6,
+			wantErr: false,
+		},
+		{
+			name: "odd",
+			args: args{
+				source: map[float64]uint64{
+					3:       4,
+					4:       6,
+					457:     2,
+					564:     1,
+					565:     1,
+					575:     1,
+					3356.56: 1,
+					35:      1,
+					45:      6,
+					46:      3,
+					47:      2,
+					56:      2,
+					57:      2,
+					64:      1,
+					65:      2,
+					5:       5,
+					6:       2,
+					7:       6,
+					9:       1,
+					23:      2,
+					34:      2,
+					74:      1,
+					345:     3,
+					436:     1,
+					453:     1,
+					456:     2,
+				},
+			},
+			want:    45,
+			wantErr: false,
+		},
+		{
+			name: "even",
+			args: args{
+				source: map[float64]uint64{
+					3:       4,
+					4:       6,
+					457:     2,
+					564:     1,
+					565:     1,
+					575:     1,
+					3356.56: 1,
+					35:      1,
+					45:      6,
+					46:      3,
+					47:      2,
+					56:      2,
+					57:      2,
+					64:      1,
+					65:      2,
+					5:       5,
+					6:       2,
+					7:       7,
+					9:       1,
+					23:      2,
+					34:      2,
+					74:      1,
+					345:     3,
+					436:     1,
+					453:     1,
+					456:     2,
+				},
+			},
+			want:    45,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := MedianFromMap(tt.args.source)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("MedianFromMap() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("MedianFromMap() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
